f9c420f350650c229e6eba2cdb9315dea6f96bb2
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-03-29 10:33:17 -0300

    Update ModeloController.java

--- src/main/java/br/com/os/backend/controller/ModeloController.java
+++ src/main/java/br/com/os/backend/controller/ModeloController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/modelos")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 
 public class ModeloController {
     private final ModeloService service;
