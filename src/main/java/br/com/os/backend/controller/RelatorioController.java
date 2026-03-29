c8daa7c9bc1066f7781f2380b4778efbfe8681c2
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-03-29 14:22:20 -0300

    Update CrossOrigin annotation to allow all origins

--- src/main/java/br/com/os/backend/controller/RelatorioController.java
+++ src/main/java/br/com/os/backend/controller/RelatorioController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/relatorios")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class RelatorioController {
 
     private final RelatorioService service;
