c56a9340c11af06733df608af71301a7f7dea880
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-03-29 11:29:07 -0300

    Add CrossOrigin annotation to ModeloController

--- src/main/java/br/com/os/backend/controller/ModeloController.java
+++ src/main/java/br/com/os/backend/controller/ModeloController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/modelos")
-
+@CrossOrigin(origins = "*")
 
 public class ModeloController {
     private final ModeloService service;
