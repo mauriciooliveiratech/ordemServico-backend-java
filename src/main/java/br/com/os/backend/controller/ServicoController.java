c801d881e57a7b36c6cb4dca6ba018f23c261253
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-03-29 10:34:07 -0300

    Allow CORS from all origins in ServicoController

--- src/main/java/br/com/os/backend/controller/ServicoController.java
+++ src/main/java/br/com/os/backend/controller/ServicoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/servicos")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ServicoController {
     private final ServicoService service;
 
